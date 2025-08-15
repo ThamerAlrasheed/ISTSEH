10baf06d169bfbe10d5e42546e2c61292f45bbce
ThamerAlrasheed <s2221190533@ku.edu.kw>
2025-08-15 18:45:28 +0300

    Initial Commit

---
+++ MEDSAITests/MEDSAITests.swift
@@ -0,0 +1,17 @@
+//
+//  MEDSAITests.swift
+//  MEDSAITests
+//
+//  Created by Mac on 15/08/2025.
+//
+
+import Testing
+@testable import MEDSAI
+
+struct MEDSAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
